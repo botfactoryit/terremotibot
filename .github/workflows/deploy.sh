9b88983f78d2689f2fa0029190fb814e58bfa608
Francesco Tonini <francesco@tonini.dev>
2022-07-27 16:59:48 +0200

    Add deploy script

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Log into AWS ECR
+echo $DOCKER_REGISTRY_TOKEN | docker login --username AWS --password-stdin $DOCKER_REGISTRY_NAME
+
+cd /home/deploy/terremotibot
+echo DOCKER_TAG=$DOCKER_TAG > .env
+echo HOSTNAME=$HOSTNAME >> .env
+docker-compose -f docker-compose.prod.yml pull
+docker-compose -f docker-compose.prod.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
+exit
