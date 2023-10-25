76c89e1397996e74e8cb0ed6c205a49f40aba2a7
Matteo Contrini <matteo@contrini.it>
2023-10-25 18:12:41 +0200

    Update deploy action
    [skip ci]

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -9,4 +9,3 @@ echo DOCKER_TAG=$DOCKER_TAG > .env
 echo HOSTNAME=$HOSTNAME >> .env
 docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
-exit
