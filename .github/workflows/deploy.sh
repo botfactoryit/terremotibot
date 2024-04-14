814389a430b9f52b930a796ddf48ec52b57c5a2c
Matteo Contrini <matteo@contrini.it>
2024-04-14 17:16:37 +0200

    Use Docker Compose 2

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,5 +7,5 @@ echo $DOCKER_REGISTRY_TOKEN | docker login --username AWS --password-stdin $DOCK
 cd /home/deploy/terremotibot
 echo DOCKER_TAG=$DOCKER_TAG > .env
 echo HOSTNAME=$HOSTNAME >> .env
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
+docker compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.prod.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
