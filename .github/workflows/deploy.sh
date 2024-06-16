8bd6270375f06005089673b8878e3c3f0a4631e8
Matteo Contrini <matteo@contrini.it>
2024-06-16 17:47:21 +0200

    Update GitHub Actions

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,8 +4,8 @@ set -e
 # Log into AWS ECR
 echo $DOCKER_REGISTRY_TOKEN | docker login --username AWS --password-stdin $DOCKER_REGISTRY_NAME
 
-cd /home/deploy/terremotibot
+cd ~/terremotibot
 echo DOCKER_TAG=$DOCKER_TAG > .env
 echo HOSTNAME=$HOSTNAME >> .env
-docker compose -f docker-compose.prod.yml pull
-docker compose -f docker-compose.prod.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
+docker compose pull
+docker compose -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
